6c74003e48f32007fd160a7cf76dcbe72b3bb4c7
Jiten Basnet <jiten.basnet@ebpearls.com>
2023-05-06 01:29:52 +0545

    :feat: fix un-used classes

--- lib/web_view.dart
+++ lib/web_view.dart
@@ -43,7 +43,7 @@ class _ReCaptchaWebViewState extends State<ReCaptchaWebView> {
       child: WebViewPlus(
         javascriptMode: JavascriptMode.unrestricted,
         onWebViewCreated: (controller) {
-            // controller.loadUrl('assets/recaptcha/index2.html');
+            // controller.loadUrl('assets/recaptcha/gindex.html');
             // Future.delayed(const Duration(seconds: 1)).then(
             //       (value) {
             //     controller.webViewController.runJavascript(
@@ -51,7 +51,7 @@ class _ReCaptchaWebViewState extends State<ReCaptchaWebView> {
             //   },
             // );
           if (_useGCaptchaV3) {
-            controller.loadUrl('assets/recaptcha/index1.html');
+            controller.loadUrl('assets/recaptcha/index.html');
             Future.delayed(const Duration(seconds: 1)).then(
                   (value) {
                 controller.webViewController.runJavascript(
@@ -59,7 +59,7 @@ class _ReCaptchaWebViewState extends State<ReCaptchaWebView> {
               },
             );
           } else {
-          controller.loadUrl('assets/webpages/index2.html');
+          controller.loadUrl('assets/webpages/gindex.html');
           Future.delayed(const Duration(seconds: 1)).then(
             (value) {
               controller.webViewController.runJavascript(
