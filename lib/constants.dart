9e6ae6556551b41de15df84889a5b9c4c7f07548
Jiten Basnet <jiten.basnet@ebpearls.com>
2023-05-12 18:56:08 +0545

    :feat: host index.html

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 class AppConstants {
   AppConstants._();
 
-  static String webPage = 'packages/flutter_gcaptcha_v3/assets/index.html';
+  static String webPage = 'https://basnetjiten.github.io/captcha_index/index.html';
   static String readyCaptcha = 'readyCaptcha';
   static String readyJsName = 'Ready';
   static String captchaJsName = 'Captcha';
